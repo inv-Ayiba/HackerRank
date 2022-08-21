d0c2351916f6e61631487fc4ee866c9bb36d44c8
inv-Ayiba <tariogounga@gmail.com>
2022-08-21 04:43:41 +0100

    using __name__

--- Project Euler #254 Sums of Digit Factorials.py
+++ Project Euler #254 Sums of Digit Factorials.py
@@ -14,4 +14,42 @@ def sf(n):
     for i in range(len(strNum)):
         sumi+=(int(strNum[i]))
     return(sumi)
-sf(234)
+
+def gfor(i,r):
+    #smallest int such that sf(n)=i
+    for n in range(r):
+        if(sf(n)==i):
+            # return("sf(",n,")=",i)
+            return(n)
+
+
+def sgfor(i,r):
+    #sum of g(i) digits
+    gg=gfor(i,r)
+    strNum=str(gg)
+    
+    sumi=0
+    try:
+        for i in range(len(strNum)):
+            sumi+=(int(strNum[i]))
+        return(sumi)
+    except(ValueError):
+        return "error! range r probably too small"
+        
+
+
+def summforsg(ra1,ra2):
+    sum=0
+    for k in range(ra1,ra2+1):
+        sum+= sgfor(k,268)
+        print(k, sgfor(k,268))
+    print(sum)
+# print(sf(25))
+print(sgfor(1,268))
+
+import time
+# summforsg(1,20)
+#### why is sf(1) 1 and  sgfor(1,268) 0
+target=sf
+print(target.__name__)
+# time.time.__name__ 
